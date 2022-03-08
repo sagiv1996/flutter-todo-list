a64428f0740bd50c45f91ea16ddd562d90b358a9
Sagiv Bonali <sagivb13@gmail.com>
2022-03-08 22:26:41 +0200

    Add suppot to back to home page after create a new note from shortcute actions

--- lib/view/page/edit_note_page.dart
+++ lib/view/page/edit_note_page.dart
@@ -3,6 +3,8 @@ import 'package:fluuter_todo_list_app/controller/controller_note.dart';
 import 'package:fluuter_todo_list_app/model/note.dart';
 import 'package:fluuter_todo_list_app/view/widget/note_form_widget.dart';
 
+import 'notes_page.dart';
+
 class AddEditNotePage extends StatefulWidget {
   final Note? note;
 
@@ -81,7 +83,14 @@ class _AddEditNotePageState extends State<AddEditNotePage> {
         addNote();
       }
 
-      Navigator.of(context).pop();
+      if (Navigator.of(context).canPop()) {
+        Navigator.of(context).pop();
+      } else {
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (BuildContext context) => NotesPage()),
+            (Route<dynamic> route) => false);
+      }
     }
   }
 
