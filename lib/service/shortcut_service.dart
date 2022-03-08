0a046f4e67b42a914755e00bbca2ddd175acd8c4
Sagiv Bonali <sagivb13@gmail.com>
2022-03-08 22:24:07 +0200

    Add listner to new note from shortcut actions

--- lib/service/shortcut_service.dart
+++ lib/service/shortcut_service.dart
@@ -2,8 +2,14 @@ import 'package:fluuter_todo_list_app/model/note.dart';
 import 'package:quick_actions/quick_actions.dart';
 
 class ShortcuteService {
-  List<ShortcutItem> returnShortcuts(List<Note> notes) =>
-      notes.map((note) => noteToShortcutItem(note)).toList();
+  List<ShortcutItem> returnShortcuts(List<Note> notes) {
+    List<ShortcutItem> shortcutItems =
+        notes.map((note) => noteToShortcutItem(note)).toList();
+    // type = -1 is mean new note
+    shortcutItems.insert(
+        0, ShortcutItem(localizedTitle: 'New note', type: '-1'));
+    return shortcutItems;
+  }
 
   ShortcutItem noteToShortcutItem(Note note) => ShortcutItem(
       type: note.id.toString(), localizedTitle: note.title, icon: 'AppIcon');
