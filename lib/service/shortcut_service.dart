6893b9664e39c46f2993b9c3373d498745969763
Sagiv Bonali <sagivb13@gmail.com>
2022-03-08 22:18:08 +0200

    Delete old code

--- lib/service/shortcut_service.dart
+++ lib/service/shortcut_service.dart
@@ -1,17 +1,7 @@
-import 'package:fluuter_todo_list_app/db/notes_database.dart';
 import 'package:fluuter_todo_list_app/model/note.dart';
 import 'package:quick_actions/quick_actions.dart';
 
 class ShortcuteService {
-  Future<List<ShortcutItem>> returnShortscute() async {
-    List<ShortcutItem> shortcutItems = [];
-    List<Note> notes = await NotesDataBase.instance.readAllNotes();
-    shortcutItems.addAll(notes.map((e) => noteToShortcutItem(e)));
-    shortcutItems
-        .add(ShortcutItem(type: 'new_note', localizedTitle: 'New note'));
-    return shortcutItems;
-  }
-
   List<ShortcutItem> returnShortcuts(List<Note> notes) =>
       notes.map((note) => noteToShortcutItem(note)).toList();
 
