b1af16b6b6c295250a7b1b7a28a2134895da25e5
Sagiv Bonali <sagivb13@gmail.com>
2022-04-09 20:59:01 +0300

    Fix a bug with shortcut

--- lib/service/shortcut_service.dart
+++ lib/service/shortcut_service.dart
@@ -3,6 +3,9 @@ import 'package:quick_actions/quick_actions.dart';
 
 class ShortcuteService {
   List<ShortcutItem> returnShortcuts(List<Note> notes) {
+    // Cut list note
+    notes = notes.sublist(0, 4);
+
     List<ShortcutItem> shortcutItems =
         notes.map((note) => noteToShortcutItem(note)).toList();
     // type = -1 is mean new note
